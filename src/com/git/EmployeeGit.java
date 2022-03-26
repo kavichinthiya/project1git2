e903f862de1990f10165d0cbdf14407949efe6e0
kavichinthiya <kavisinthiyaa@gmail.com>
2022-03-26 17:11:52 +0530

    git installation

---
+++ src/com/git/EmployeeGit.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class EmployeeGit {
+	public void add() {
+		System.out.println("kavi");
+
+	}
+	public static void main(String[] args) {
+		EmployeeGit e  = new EmployeeGit();
+		e.add();
+		
+	}
+
+}
